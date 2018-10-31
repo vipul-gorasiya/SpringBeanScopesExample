439ad97daa27711c0ac5b4660ab25ce82ff7de06
vipul-gorasiya <39234040+vipul-gorasiya@users.noreply.github.com>
2018-10-31 15:21:52 +0530

    Added example of  having singleton in differnt containers

--- SpringBeanScopesExample/src/main/java/com/vipul/SpringBeanScopesExampleApplication.java
+++ SpringBeanScopesExample/src/main/java/com/vipul/SpringBeanScopesExampleApplication.java
@@ -2,17 +2,29 @@ package com.vipul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import com.vipul.service.SingletonScopedService;
 
 @SpringBootApplication
 @EnableWebSocketMessageBroker
 public class SpringBeanScopesExampleApplication implements WebSocketMessageBrokerConfigurer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBeanScopesExampleApplication.class, args);
+		ConfigurableApplicationContext applicationContext1 = SpringApplication
+				.run(SpringBeanScopesExampleApplication.class, args);
+		// Created another container which will have its own instance of singleton bean
+		ConfigurableApplicationContext applicationContext2 = new AnnotationConfigApplicationContext(SingletonScopedService.class);
+		SingletonScopedService sbean = applicationContext1.getBean(SingletonScopedService.class);
+		SingletonScopedService sbean2 = applicationContext2.getBean(SingletonScopedService.class);
+		System.out.println("SingletonScopedService instance from applicationContext1 : " + sbean.toString());
+		System.out.println("SingletonScopedService instance from applicationContext2 : " + sbean2.toString());
+		applicationContext2.close();
 	}
 
 	@Override
